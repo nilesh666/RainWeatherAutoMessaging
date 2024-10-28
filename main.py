50d6c03f5919408b983aed42dd424ebad9a3c965
NEELU <nileshnandan.ts@gmail.com>
2024-10-28 16:10:38 +0530

    Initial commit with main script and dependencies
    - Added main.py with initial implementation
    - Included requirements.txt for dependencies
    - Provided .env_sample for environment variable configuration

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv, dotenv_values
+import requests
+from twilio.rest import Client
+
+load_dotenv()
+
+api = os.environ.get("WEATHER_API")
+city = os.environ.get("CITY")
+account_sid = os.environ.get("TWILIO_ACC_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+ph1 = os.environ.get("PERSON_PH_NO")
+ph2 = os.environ.get("TRIAL_PH_NO")
+
+url= f"https://api.weatherapi.com/v1/forecast.json?key={api}&q={city}&days=2&aqi=no&alerts=no"
+response = requests.get(url)
+data = response.json()
+
+
+if data['forecast']['forecastday'][1]['day']['daily_will_it_rain']:
+    msg1 = "Tomorrow go with an umbrella mate....!"
+    msg = msg1+ f"Possiblilty of rain is {data['forecast']['forecastday'][1]['day']['daily_chance_of_rain']}%"
+    client = Client(account_sid, auth_token)
+
+    m = client.api.account.messages.create(
+        to="+919626321666",
+        from_="+17606210294",
+        body= msg)
+# else:
+#     print("No, there is no rain")
+print(m.status)
